a4ec96bde4de25401bf5c880f05fa597a77ba7bc
Tien Ngo <ngoquoctien24196@gmail.com>
2022-06-23 01:07:12 -0400

    add function to ask for types of characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,27 @@ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numBer = "0123456789";
 var specChar = "!#$%&'()*+,-./:;<=>?@][^_`{|}~";
 
+function generatePassword() {
+
+  var passLength = window.prompt("How many characters do you want in your password?");
+
+  if(passLength >= 8 && passLength <= 128) {
+    
+    var pickedLowercase = window.confirm("Do you want to include lowercase characters in your password?")
+    var pickedUppercase = window.confirm("Do you want to include uppercase characters in your password?")
+    var pickedNumber = window.confirm("Do you want to include number characters in your password?")
+    var pickedSpechar = window.confirm("Do you want to include special characters in your password?")
+
+    while(pickedLowercase === false && pickedUppercase === false && pickedNumber === false && pickedSpechar === false) {
+      window.alert("You must choose at least one character type!")
+      var pickedLowercase = window.confirm("Do you want to include lowercase characters in your password?")
+      var pickedUppercase = window.confirm("Do you want to include uppercase characters in your password?")
+      var pickedNumber = window.confirm("Do you want to include number characters in your password?")
+      var pickedSpechar = window.confirm("Do you want to include special characters in your password?")
+        
+  } 
+}};
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
